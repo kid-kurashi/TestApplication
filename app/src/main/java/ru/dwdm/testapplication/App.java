e67e2d56d108edad50460e940bb5a381e77a14be
Бункевич Дмитрий <night.coal@gmail.com>
2019-03-05 14:48:28 +0700

    Зависимости, Верстка экрана Preview

---
+++ app/src/main/java/ru/dwdm/testapplication/App.java
@@ -0,0 +1,11 @@
+package ru.dwdm.testapplication;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
